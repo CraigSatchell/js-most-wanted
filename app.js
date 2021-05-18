fc10943d568a478b21865d464aef7eb3566d3376
Craig Satchell <satchdev@gmail.com>
2021-05-18 19:44:46 -0400

    add various input; ui mods - ccs

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ function mainMenu(person, people) {
 
   switch (displayOption) {
     case "info":
-      // Craig TODO -  : get person's info
+      // Craig DONE: get person's info
       // create function to print out person info
       displayPerson(person[0], people)
       break;
@@ -85,26 +85,6 @@ function displayPeople(people) {
   }).join("\n"));
 }
 
-function displayPerson(person, people) {
-  // print all of the information about a person:
-  // height, weight, age, name, occupation, eye color.
-  let personInfo = "First Name: " + person.firstName + "\n";
-  personInfo += "Last Name: " + person.lastName + "\n";
-  personInfo += "Gender: " + person.gender + "\n";
-  personInfo += "DOB: " + person.dob + "\n";
-  personInfo += "Height: " + person.height + "\n";
-  personInfo += "Weight: " + person.weight + "\n";
-  personInfo += "Eye Color: " + person.eyeColor + "\n";
-  personInfo += "Occupation: " + person.occupation + "\n";
-  personInfo += "Parents: " + retrievePersonName(person.parents[0], people) + ", " + retrievePersonName(person.parents[1], people) + "\n";
-  personInfo += "Current Spouse: " + retrievePersonName(person.currentSpouse, people) + "\n";
-
-  // Craig DONE: finish getting the rest of the information to display
-
-  alert(personInfo);
-}
-
-
 
 
 // function that prompts and validates user input
@@ -126,32 +106,11 @@ function chars(input) {
 }
 
 
-/*****************************************
- * paired programming helper functions
- */
 
-function lookupPersonByID(id, people) {
-  let person = people.find(function (person) {
-    if (person.id === id) {
-      return person;
-    } else {
-      return undefined;
-    }
-  });
-  console.log('selected: ', person);
-  return person; // person
-}
+/***********************************************************
+ * paired programming
+************************************************************/
 
-function retrievePersonName(id, people) {
-  let person = lookupPersonByID(id, people);
-  let name = "unknown";
-  if (person) {
-    name = person.firstName + " " + person.lastName;
-  }
-  console.log('retrieve: ', person);
-  console.log(name);
-  return name;
-}
 //(completed) searchByTraits() Giancarlo
 function searchByTraits(people) {
   let gender = promptFor("What is the person's gender", chars); //string
@@ -171,3 +130,132 @@ function searchByTraits(people) {
   return foundPerson;
 }
 
+
+// display person information - Craig
+function displayPerson(person, people) {
+  // print all of the information about a person:
+  // height, weight, age, name, occupation, eye color.
+  let personInfo = "Name: " + person.firstName + " " + person.lastName + "\n";
+  personInfo += "Gender: " + person.gender + "\n";
+  personInfo += "DOB: " + person.dob + "\n";
+  personInfo += "Height: " + person.height + "\n";
+  personInfo += "Weight: " + person.weight + "\n";
+  personInfo += "Eye Color: " + person.eyeColor + "\n";
+  personInfo += "Occupation: " + person.occupation + "\n";
+  personInfo += "Parents: " + retrievePersonName(person.parents[0], people) + ", " + retrievePersonName(person.parents[1], people) + "\n";
+  personInfo += "Current Spouse: " + retrievePersonName(person.currentSpouse, people) + "\n";
+
+  // Craig DONE: finish getting the rest of the information to display
+
+  alert(personInfo);
+}
+
+
+
+// TODO - display a person's descendants
+function displayDescendants() {
+
+}
+
+
+
+// TODO - display a person's family
+function displayFamily() {
+
+}
+
+
+
+// perform person lookup using id - Craig
+function lookupPersonByID(id, people) {
+  let person = people.find(function (person) {
+    if (person.id === id) {
+      return person;
+    } else {
+      return undefined;
+    }
+  });
+  console.log('selected: ', person);
+  return person; // person
+}
+
+
+// return string containing person name
+// from lookup - Craig
+function retrievePersonName(id, people) {
+  let person = lookupPersonByID(id, people);
+  let name = "unknown";
+  if (person) {
+    name = person.firstName + " " + person.lastName;
+  }
+  console.log('retrieve: ', person);
+  console.log(name);
+  return name;
+}
+
+
+// lookup descendants by id ( children and grand children ) - Craig
+function lookupDescendants(id, people) { //lookup (children, grand children, and great grand children )
+  let offSpring = [];
+  for (let i = 0; i < people.length; i++) {
+    if (people[i].parents[0] === id || people[i].parents[1] === id) {
+      offSpring.push({ person: people[i], relation: 'child' });
+      for (let j = 0; j < people.length; j++) {
+        if (people[j].parents[0] === offSpring[offSpring.length - 1].person.id || people[j].parents[1] === offSpring[offSpring.length - 1].person.id) {
+          offSpring.push({ person: people[j], relation: 'grand-child' });
+          for (let k = 0; k < people.length; k++) {
+            if (people[k].parents[0] === offSpring[offSpring.length - 1].person.id || people[k].parents[1] === offSpring[offSpring.length - 1].person.id) {
+              offSpring.push({ person: people[k], relation: 'great-grand-child' });
+            }
+          }
+
+        }
+      }
+    }
+  }
+  return offSpring; // array of object
+}
+
+
+// lookup siblings by id - Craig
+function lookupSiblings(id, people) {
+  let siblings = [];
+  let person = lookupPersonByID(id, people);
+  let found = false;
+  for (let i = 0; i < people.length; i++) {
+    if (person.parents.length != 0) {
+      if (((person.parents[0] !== undefined) && person.parents[0] === people[i].parents[0] || person.parents[0] === people[i].parents[1]) && people[i].id != id) {
+        found = true;
+      } else if (((person.parents[1] !== undefined) && person.parents[1] === people[i].parents[0] || person.parents[0] === people[i].parents[1]) && people[i].id != id) {
+        found = true;
+      } else {
+        found = false;
+      }
+      found ? siblings.push(people[i]) : siblings // append sibling
+    }
+  }
+  return siblings;
+}
+
+
+// entire family matrix for selected person - Craig
+function familyPerson(id, people) {
+  let person;
+  let parent1 = {};
+  let parent2 = {};
+  let descendants = [];
+  let siblings = [];
+  let currentSpouseDetails = {};
+
+  person = lookupPersonByID(id, people);
+  parent1 = lookupPersonByID(person.parents[0], people);
+  parent2 = lookupPersonByID(person.parents[1], people);
+  descendants = lookupDescendants(id, people);
+  siblings = lookupSiblings(id, people)
+  spouseDetails = lookupPersonByID(person.currentSpouse, people);
+  return { ...person, descendants, siblings, currentSpouseDetails, parent1, parent2 };
+}
+
+
+
+ 
